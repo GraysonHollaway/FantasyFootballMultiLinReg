d7c1a97a4e817ff67b070c12f2eceea004e4357d
Grayson Hollaway <graysonjayhollaway@gmail.com>
2022-09-03 18:52:17 -0400

    Added README and notebook

---
+++ README.md
@@ -0,0 +1,10 @@
+First-order multiple linear regression implemented in python
+--------------------------------------------------------------
+Check out main.ipynb for main method and data cleaning.
+All data is scraped from pro football focus in the
+python script.
+--------------------------------------------------------------
+TODO:
+-Use one-hot encoding to factor in position of each player and 
+team for which they play
+-Iterate through more years to train model
--- scrape_pro_football_reference.py
+++ scrape_pro_football_reference.py
@@ -2,22 +2,21 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 
-#maybe put a loop here for more years
-year = 2021 
-url = "https://www.pro-football-reference.com/years/{}/fantasy.htm".format(year)
-html = urlopen(url)
-soup = BeautifulSoup(html, features="html.parser")
 
-headers = [th.getText() for th in soup.findAll("tr")[1].findAll("th")]
-headers = headers[1:]
-print(len(headers))
 
-rows = soup.findAll("tr", class_= lambda table_rows: table_rows != "thread")
-player_stats = [[td.getText() for td in rows[i].findAll('td')] for i in range(len(rows))] 
-player_stats  = player_stats[2:]
+def get_player_df(year):
+    #maybe put a loop here for more years
+    url = "https://www.pro-football-reference.com/years/{}/fantasy.htm".format(year)
+    html = urlopen(url)
+    soup = BeautifulSoup(html, features="html.parser")
 
-print(player_stats[:5])
+    headers = [th.getText() for th in soup.findAll("tr")[1].findAll("th")]
+    headers = headers[1:]
 
-stats = pd.DataFrame(player_stats, columns=headers)
+    rows = soup.findAll("tr", class_= lambda table_rows: table_rows != "thread")
+    player_stats = [[td.getText() for td in rows[i].findAll('td')] for i in range(len(rows))] 
+    player_stats  = player_stats[2:]
 
-stats.to_csv("2021playerstats.csv")
+    stats = pd.DataFrame(player_stats, columns=headers)
+
+    return stats
